55d660e07a5315d35a393ddc44f5483bdffb38a0
Lewis Parmenter <lewis.o.parmenter@gmail.com>
2023-04-07 08:57:31 +1000

    Added simple MVVM principles.

---
+++ FocusPad/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+using System.Windows;
+using System.Windows.Input;
+
+namespace FocusPad.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public ICommand CommandOpenSettings { get; private set; }
+        public ICommand CommandExitProgram { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            CommandOpenSettings = new RelayCommand(OpenSettings);
+            CommandExitProgram = new RelayCommand(ExitProgram);
+        }
+
+        private void OpenSettings()
+        {
+        }
+
+        private void ExitProgram()
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
