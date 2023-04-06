99777d3b1435c6ba59a2478f553232d0e14932bd
Lewis Parmenter <lewis.o.parmenter@gmail.com>
2023-04-07 09:57:22 +1000

    Added keyboard hook.

--- FocusPad/Views/MainWindow.xaml.cs
+++ FocusPad/Views/MainWindow.xaml.cs
@@ -1,15 +1,28 @@
-﻿using FocusPad.ViewModels;
+﻿using FocusPad.Utils;
+using FocusPad.ViewModels;
 using System.Windows;
+using System.Windows.Forms;
 
 namespace FocusPad.Views
 {
     public partial class MainWindow : Window
     {
+        private KeyboardHook keyHook;
+
         public MainWindow()
         {
             InitializeComponent();
 
             this.Visibility = Visibility.Hidden;
+
+            keyHook = new KeyboardHook();
+            keyHook.RegisterHotKey(ModifierKeys.Alt | ModifierKeys.Shift, Keys.R);
+            keyHook.KeyPressed += KeyHook_KeyPressed;
+        }
+
+        private void KeyHook_KeyPressed(object? sender, KeyPressedEventArgs e)
+        {
+            this.Visibility = Visibility.Visible;
         }
 
         private void FocusPadMainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
